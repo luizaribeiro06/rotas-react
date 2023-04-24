e8902c88bf2d4919ed53576d7fd2801df312cd92
Luiza Ribeiro <84139409+luizaribeiro06@users.noreply.github.com>
2023-04-24 12:27:37 -0300

    Add files via upload

---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,15 @@
+import Menu from "./Menu"
+
+function Sobre() {
+  
+    return (
+      
+        <div>
+            <h1>Sobre</h1>
+            <Menu/>
+        </div>
+      
+    )
+  }
+  
+  export default Sobre
